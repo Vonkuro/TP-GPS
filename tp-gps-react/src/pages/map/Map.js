a02ce86ed5431cee8c43ca495260a8828d5297f5
McLouffy <119299314+McLouffy@users.noreply.github.com>
2023-01-21 21:28:59 +0100

    ajout carte et marqueurs

--- tp-gps-react/src/pages/map/Map.js
+++ tp-gps-react/src/pages/map/Map.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './Map.css';
+import cities from '../data/cities.json'
 import { MapContainer, TileLayer,Marker, Popup } from 'react-leaflet';
 
 function Map(){
+    console.log(cities)
+    const positionLille = [50.629250, 3.057256]
     return (
+        
 
-        <MapContainer center={[50.629250, 3.057256]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={positionLille} zoom={13} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[50.629250, 3.057256]}>
-                <Popup>
-                     A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            {cities.map(villes=>(
+                <Marker key={villes.id}
+                position={[villes.gps.latitude, villes.gps.longitude]}>
+                </Marker>
+
+            ))} 
+            
+            
         </MapContainer>
 
     )
