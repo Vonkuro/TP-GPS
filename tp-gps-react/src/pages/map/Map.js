5f111aa8b6052acb6b7c48f80289891260109a3a
McLouffy <119299314+McLouffy@users.noreply.github.com>
2023-01-22 10:49:57 +0100

    gestion routing

--- tp-gps-react/src/pages/map/Map.js
+++ tp-gps-react/src/pages/map/Map.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Map.css';
-import cities from '../data/cities.json'
-import { MapContainer, TileLayer,Marker, Popup } from 'react-leaflet';
+import cities from '../data/cities.json';
+import { MapContainer, TileLayer,Marker } from 'react-leaflet';
+import Routing from './Routing';
 
 function Map(){
     console.log(cities)
     const positionLille = [50.629250, 3.057256]
     return (
+
+        <div className='map-container'>
         
 
         <MapContainer center={positionLille} zoom={13} scrollWheelZoom={false}>
@@ -14,15 +17,16 @@ function Map(){
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {cities.map(villes=>(
+            {/* {cities.map(villes=>(
                 <Marker key={villes.id}
                 position={[villes.gps.latitude, villes.gps.longitude]}>
                 </Marker>
 
-            ))} 
-            
-            
+            ))}  */}
+            <Routing />
         </MapContainer>
+
+    </div>
 
     )
 
